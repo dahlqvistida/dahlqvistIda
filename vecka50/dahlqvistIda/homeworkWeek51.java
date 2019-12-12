027af46cfec205fdfffc9f01938b044252a2810b
dahlqvistida <55230391+dahlqvistida@users.noreply.github.com>
2019-12-12 20:35:14 +0100

    Update homeworkWeek51.java
    I have commented everything the code does

--- vecka50/dahlqvistIda/homeworkWeek51.java
+++ vecka50/dahlqvistIda/homeworkWeek51.java
@@ -6,70 +6,105 @@ public class homeworkWeek51 {
 
 	public static void main(String[] args) {
 		
+		//Creating a scanner to get input from the console
 		Scanner input = new Scanner(System.in);
 
-		System.out.println("Choose an what you would want to bake");
+		//Printing out information about what choices the user can make
+		System.out.println("Choose what you would want to bake");
 		System.out.println("(1) - Xmas candy");
 		System.out.println("(2) - Something Vegan");
 		System.out.println("(3) - Something with Chocolate");
 		System.out.println("(4) - Candy");
 		System.out.println("(5) - Quit");
+		//Prints out an empty line to make the console look more structured 
 		System.out.println();
 
+		//Printing out text about what choice the user makes
+		System.out.print("Your choice is: ");
+		
+		//Creating an integer where the user writes the number for the choice
 		int choice = input.nextInt();
 
-		String genre;
+		//Creating a String variable for the pastries (bakverk)
+		String pastry;
 
+		
+		//Creating a switch statement and all cases for the users choice 
 		switch (choice) {
+		//Case 1 prints out different types of xmas candy recipes
 		case 1:
-			genre = "Xmas candy";
-			System.out.println("You chose " + genre );
-			System.out.println("Here is my favorite xmas candy ");
+			pastry = "Xmas candy";
+			System.out.println("You chose " + pastry );
+			System.out.println("Here are my favorite xmas candy ");
+			//Prints out an empty line to make the console look more structured 
 			System.out.println();
 			System.out.println("1. Candy Cane Chocolate Swirl Fudge");
 			System.out.println("2. Gingerbread House White Chocolate Bark");
 			System.out.println("3. Peanut Brittle");
 			System.out.println();
+			//This is to stops this switch
 			break;
+			
+		//Case 2 prints out different types of vegan recipes
 		case 2:
-			genre = "Something Vegan";
-			System.out.println("You chose " + genre);
-			System.out.println("Here is some vegan recipies ");
+			pastry = "Something Vegan";
+			System.out.println("You chose " + pastry);
+			System.out.println("Here are some vegan recipies ");
+			//Prints out an empty line to make the console look more structured 
 			System.out.println();
 			System.out.println("1. Vegan choclate Chip Cookies");
 			System.out.println("2. Keto Peanut Butter Cookies");
 			System.out.println("3. Vegan Unicorn Cookies");
 			System.out.println();
+			//This is to stops this switch
 			break;
+			
+		//Case 2 prints out different types of chocolate pastry recipes
 		case 3:
-			genre = "something with chocolate";
-			System.out.println("You chose " + genre);
+			pastry = "something with chocolate";
+			System.out.println("You chose " + pastry);
+			System.out.println("Here are some chocolate recipies");
+			//Prints out an empty line to make the console look more structured 
 			System.out.println();
+			System.out.println("1. Swedish Kladdkaka");
+			System.out.println("2. Chocolate cupcake");
+			System.out.println("3. Chocolate fudge");
 			System.out.println();
-			System.out.println("1. The notebook, av Nick Cassavetes");
-			System.out.println("2. The holidays, av Nancy Meyrs");
-			System.out.println("3. Love Simon, av Greg Berlanti");
-			System.out.println();
+			//This is to stops this switch
 			break;
+			
+		//Case 2 prints out different types of candy recipes
 		case 4:
-			genre = "Fantasy";
-			System.out.println("You chose " + genre + "! Here are three good movies!");
+			pastry = "candy";
+			System.out.println("You chose " + pastry);
+			System.out.println("Here are some candy recepis");
+			//Prints out an empty line to make the console look more structured 
 			System.out.println();
-			System.out.println("1. Lord of the rings - Sagan om ringen, av Peter Jackson");
-			System.out.println("2. Aquaman, av James Wan");
-			System.out.println("3. Avengers - Infinity war, av Anthony & Joe Russo");
+			System.out.println("1. Marshmallows");
+			System.out.println("2. Maple Fondant");
+			System.out.println("3. Strawberry Lollipop");
 			System.out.println();
+			//This is to stops this switch
 			break;
+		
+		//Case 5 prints out a goodbye phrase because the user chosed to quit
 		case 5:
-			System.out.println("Thank you for today!");
+			System.out.println("That's your loss, Thank you for today!");
+			//This is to stops this switch
 			break;
+		
+		//If the user writes an incorrect number this one tells it to write it correct
 		default:
-			genre = "invalid";
-			System.out.println("You have made a " + genre + " choice! Too bad for you!");
+			pastry = "invalid";
+			System.out.println("You have made a " + pastry + " choice!");
+			System.out.println("Make a correct choice please");
 			System.out.println();
+			//This is to stops this switch
 			break;
+			
 		}
-
+		
+		//Closes the scanner input
 		input.close();
 	}
 
