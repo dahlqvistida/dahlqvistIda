1baa86119995c2293c8251db049cc445a286b0e1
dahlqvistida <55230391+dahlqvistida@users.noreply.github.com>
2020-02-25 11:13:39 +0100

    Update bubbleSort.java

--- vecka3/dahlqvistIda/bubbleSort.java
+++ vecka3/dahlqvistIda/bubbleSort.java
@@ -1,20 +1,38 @@
 package dahlqvistIda;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class bubbleSort {
-
+				//bubbleSort sorterar massa data
 	public static void main(String[] args) {
 		
-		//Vi skapar en array med 37 platser
-		int []  numbers = new int[37];
+		Scanner input = new Scanner(System.in);
+		
+		//Användaren får bestämma hur många platser arreyen skall ha.
+		System.out.println("Hur många platser skall det finnas? ");
+		
+		
+		int lenght = input.nextInt();
+		int []  numbers = new int[lenght];
+		System.out.println("Såhär många platser finns det: " + lenght);
+		System.out.println();
 
 		boolean swapped = false;
 		int temp;
 		
+		
+		System.out.println("Skriv två tal som den skall välja mellan");
+		
+		int choice = input.nextInt();
+		int choice2 = input.nextInt(); 
+		System.out.println("Du valde detta intervallet " + choice + " och " + choice2);
+		choice2 += 1;
+		
+		
 		//Vi slumpar fram heltal att fylla arrayen. Tal från 0 till 100
-		for (int i = 0; i < numbers.length; i++) {
-			numbers[i] = (int) (Math.random() * 100); 
+		for (int i = choice; i < numbers.length; i++) {
+			numbers[i] = (int) (Math.random() * choice2); 
 		}
 		
 		System.out.println(Arrays.toString(numbers));
@@ -22,7 +40,7 @@ public class bubbleSort {
 		do {
 			
 			swapped = false;
-		for (int i = 0; i < numbers.length - 1; i++) {
+		for (int i = choice; i < numbers.length - 1; i++) {
 			
 			//byter plats på två intill liggande tal
 			if (numbers[i] > numbers[i+1]) { 
