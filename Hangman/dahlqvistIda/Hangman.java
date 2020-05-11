5c7100c6d8a97c50078ed673b9c4fd2ff2b6943b
dahlqvistida <55230391+dahlqvistida@users.noreply.github.com>
2020-05-11 18:22:26 +0200

    Update Hangman.java
    Kommenterat det som återstod

--- Hangman/dahlqvistIda/Hangman.java
+++ Hangman/dahlqvistIda/Hangman.java
@@ -1,6 +1,5 @@
 package dahlqvistIda;
 
-import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -59,10 +58,13 @@ public class Hangman {
 			//Spelaren väljer level
 			int difficulty = input.nextInt();
 			
+			
 			switch (difficulty) {
+			
 			//om spelaren väljer level 1 så körs case 1
 			case 1:
 				
+				//Berättar vilken nivå man valt
 				System.out.println("Du har valt den enklaste leveln");
 				
 				//så många ord det är i listan, - 1 eftersom arrayen börjar på plats 0.(Så många platser det finns o välja mellan)
@@ -78,13 +80,19 @@ public class Hangman {
 				//tidigare har tagits fram.
 				hiddenWord = shortList[arrayNumber];
 				
-				convertString(hiddenWord);
+				//Metod med hela gissnings-processen 
+				guessAtWord(hiddenWord);
 				
+				//När spelet är slut så gör break att man ut ur switch-case
 				break;
+				
+				
 			//om spelaren väljer level 2 så körs case 2
 			case 2: 
 				
+				//Berättar vilken nivå man valt
 				System.out.println("Du har valt medel-nivån");
+				
 				//så många ord det är i listan, - 1 eftersom arrayen börjar på plats 0.(Så många platser det finns o välja mellan)
 				endArrayNumber = 10-1;
 				
@@ -98,11 +106,19 @@ public class Hangman {
 				//tidigare har tagits fram.
 				hiddenWord = longList[arrayNumber];
 				
-				convertString(hiddenWord);
+				//Metod med hela gissnings-processen
+				guessAtWord(hiddenWord);
 				
+				//När spelet är slut så hoppar man ut ur switch-case
 				break;
+				
+				
 			//om spelaren väljer level 3 så körs case 3
 			case 3:
+				
+				//Berättar vilken nivå man valt
+				System.out.println("Du har valt svåraste nivån");
+				
 				//så många ord det är i listan, - 1 eftersom arrayen börjar på plats 0. (Så många platser det finns o välja mellan)
 				endArrayNumber = 5-1;
 				
@@ -111,36 +127,41 @@ public class Hangman {
 				//på den platsen i arrayen finns ordet spelaren skall gissa på 
 				arrayNumber = Interval(startArrayNumber, endArrayNumber, arrayNumber);
 				
-				
-				
 				//tar fram själva ordet man skall gissa på med hjälp av array platsen som
 				//tidigare har tagits fram.
 				hiddenWord = veryLongList[arrayNumber];
 				
-				convertString(hiddenWord);
+				//Metod med hela gissnings-processen
+				guessAtWord(hiddenWord);
 				
+				//När spelet är slut så hoppar man ut ur switch-case
 				break;
 			default:
 				break;
 			}
 		
+			//Frågar om spelaren vill spela igen
 			System.out.println("Vill du spela igen?");
 			System.out.println("(1) - Ja");
 			System.out.println("(2) - Nej");
 			
+			//Spelarens input som svar 
 			int playAgain = input.nextInt();
 			
 			switch (playAgain) {
+			
+			//Om spelaren skriver 1 så vill den spela igen, och case 1 körs
 			case 1:
 				
 				System.out.println("Vad skoj! Lycka till!");
 				
 				break;
-			
+			//Om spelaren skriver 2 så vill den avsluta, och case 2 körs
 			case 2:
 				
 				System.out.println("Vad tråkigt, kom tillbaka snart!");
 				
+				//Ändrar run till false så loopen som kör spelet skall stanna.
 				run = false; 
 				
 				break;
@@ -148,6 +169,7 @@ public class Hangman {
 				break;
 			}
 			
+		//Sålänge run = true så körs while-loopen
 		} while (run);
 		
 		
@@ -176,7 +198,7 @@ public class Hangman {
 	 * Låter användaren gissa på bokstäver till hiddenWord
 	 * @param hiddenWord
 	 */
-	private static void convertString(String hiddenWord) {
+	private static void guessAtWord(String hiddenWord) {
 		
 		
 		//Skapar en array som har lika många platser som hiddenWord har bokstäver
@@ -203,41 +225,55 @@ public class Hangman {
        	 
        	 a++;
         }
+        
+        //Skapar en int vid namn characters, som står för hur många bokstäver hiddenWord har
         int characters = 0;
         
         //för varje bokstav(c) i arrayen så skrivs "_" ut för att visa hur många bokstäver det skall gissas på.  
         for(char c : charGuessAtThisWord) {
         	 
+        	//Skriver "_" för varje bokstav i hiddenWord
         	 System.out.print("_ ");
         	 
+        	 //För varje bokstav hiddenWord har så ökar characters med +1
         	 characters++;
         	 
          }
+        
+        //Skriver ut hur många bokstäver ordet har
         System.out.println("Ordet har " + characters + " bokstäver");
         System.out.println();
 
+        //Loop som körs sålänge runGuessing = true vilket är samma som om antal gissningar (guesses) är större än noll
+        //Denna loop är för gissningarna 
          do {
         	 
         	 System.out.println("Gör en gissning");
 			
+        	 //Användaren gör en gissning på en bokstav
         	 letter = input.next().charAt(0);
         	 
-
+        	 //om hiddenWord inehåller bokstaven så körs if-satsen
         	 if(hiddenWord.contains(letter + "")) {
         		
-        		
+        		/*För varje bokstav i charArrayen charGuessAtThisWord så jämför den vilka platser som bokstaven är samma som -
+        		 i arrayen */
         		 for(int i = 0; i < charGuessAtThisWord.length; i++) {
         			 
+        			 
+        			 //Om en bokstav i hiddenWord är samma som den gissade bokstaven så ersätts '-' av den bokstaven
         				if(hiddenWord.charAt(i) == letter) {
-        				
+        					
         					charGuessedLetters[i] = letter;
         					
            				 }
-        				
         		 	}
         		 
+        		 //Printar ut charGuessedLetters, tex: -l-o---
         		 System.out.println(charGuessedLetters);
         		 
+        	//Om inte den gissade bokstaven letter finns i hiddenWord så körs else-satsen
+        	//Den tar bort en gissning och skriver hur många gissningar användaren har kvar.
         	 }else {
         		 System.out.println("Du gissade fel.");
         		 
@@ -247,23 +283,30 @@ public class Hangman {
         		 System.out.println();
         	 }
         		 
+        	 //Om hiddenWord och charGuessedLetters inehåller exakt samma bokstäver så körs if-satsen
+        	 //den skriver ut att man har vunnit
         	 if(hiddenWord.equals(String.valueOf(charGuessedLetters))) {
         					
-        		 System.out.println();
-        					System.out.println("Du vann!");
-        					System.out.println("Ordet var: " + hiddenWord);
-        					System.out.println();
+        		System.out.println();
+        		System.out.println("Du vann!");
+        		System.out.println("Ordet var: " + hiddenWord);
+        		System.out.println();
         					
-        					runGuessing = false;
-        				}
+        		//Eftersom det inte finns något mer att gissa på så ändrar den runGuessing till false så loopen skall sluta köras
+        		runGuessing = false;
+        	}
         				 
-        			 
+        	//Om man inte har några gissningar kvar så körs denna
+        	 //Den berättar att man har förlorat
         	 if(guesses == 0) {
         		 
         		 System.out.println("Du förlora!");
+        		 
+        		 //Eftersom man inte har några fler gissningar så ändrar den runGuessing till false så loopen skall sluta köras
         		 runGuessing = false;
         	 }
-        	 
+        
+        //När runGuessing = false så stannar loopen. 
 		} while (runGuessing);
          
 }
